5e40708d5240ad795637d7d4222de9a5099d3b6a
Ying-Ting Lin <claire4621@gmail.com>
2016-03-06 21:28:40 -0600

    Update get_url.py

--- get_url.py
+++ get_url.py
@@ -1,3 +1,11 @@
+#Reference: Intro to Computer Science Lesson 1- 3, Udacity
+seed = ['','',''] #seed pages
+
+#transform url to HTML source code 
+def get_page(page): 
+    
+  
+
 def get_next_target(page):
     start_link = page.find('<a href =')
     if start_link ==-1
@@ -11,14 +19,32 @@ def get_next_target(page):
 
     page = page[end_quote:]
     
-def print_all_links(page):
+def get_all_links(page):
+    links = []   #empty list
     while page!="":
-        url, end_point = get_next_target(page)
-        if url:
-            print url
-            page = page[end_point:]
-        else:
-            break
+    url, end_point = get_next_target(page)
+    if url:
+        links.append(url)
+        page = page[end_point:]
+    else:
+        break
+    return links
 
-page = ""  #Insert HTML source code 
-print_all_links(page)
+def union(a,b):#assume a is already a set, i.e.,no repetitive element
+    for e in b:
+        if e not in a:
+            a.append(e)
+
+def crawl_web(seed):
+    tocrawl = [seed]
+    crawled = []
+    while tocrawl:#if tocrawl ==[], return FALSE
+        page = tocrawl.pop() #Depth-Furst Search: the last link is searched first
+        if page not in crawled:
+            union(tocrawl, get_all_links(get_page(page)))
+            crawled.append(get_page(page))
+    return crawled
+        
+
+
+print get_all_links(get_page(page))
