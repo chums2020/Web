11f3bf01866ed4a7026feb6057a0eac92da2f0d1
Ying-Ting Lin <claire4621@gmail.com>
2016-03-07 18:00:28 -0600

    Update get_url.py

--- get_url.py
+++ get_url.py
@@ -35,22 +35,42 @@ def union(a,b):#assume a is already a set, i.e.,no repetitive element
         if e not in a:
             a.append(e)
 
-def crawl_web(seed, max_pages, max_depth):
-    tocrawl = [seed]
-    crawled = []
-    next_depth = []
-    depth = 0
-    while depth<= max_depth and len(crawled)<max_pages:#if tocrawl ==[], return FALSE
-        page = tocrawl.pop() #Depth-Furst Search: the last link is searched first
-        if page not in crawled:
-            union(next_depth, get_all_links(get_page(page)))
-            crawled.append(get_page(page))
-        if not tocrawl: #if tocrawl if empty
-            tocrawl, next_depth = next_depth, []
-            depth = depth +1
-            if next_depth ==[]:
-                break
-    return crawled
+#an index is a list [[keyword1,[url1,url2,...]],[keyword2,[url1,url2,...]],...]
+#add_to_index add the keyword and associated url to index if the keyword is not already in the index
+
+def add_to_index(index,keyword,url):
+    i=0
+    for e in index:
+        if keyword in e:
+            index[i][1].append(url)
+            return
+        i = i+1
+    index.append([keyword,[url]])
+
+#lookup returns the list of urls associated with the keyword
+
+def lookup(index,keyword):
+    for e in index:
+        if e[0]==keyword:
+            return e[1]
+    return []
+
+#add_page_to_index add the web content to the index
+
+def add_page_to_index(index,url,content):
+    words = content.split() #split the web content into a list of words
+    for word in words:
+        add_to_index(index,word,url)
+    return index
+
+
+index = []
+add_page_to_index(index,"https://www.edx.org/course/introduction-linux-linuxfoundationx-lfs101x-2#!","""
+Never learned Linux? Want a refresh? 
+Take this course free or get a verified certificate for $99 
+""")
+
+
 
 def crawl_web(seed, max_pages, max_depth):
     tocrawl = [seed]
