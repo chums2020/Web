28b8f530b04935311c948e8c8ee7d92782141c54
Ying-Ting Lin <claire4621@gmail.com>
2016-04-13 23:14:11 -0500

    Create open_hours.php

---
+++ PHP_exercise/open_hours.php
@@ -0,0 +1,30 @@
+<!--Display today's working hours-->
+<html>
+	<body>
+		<h1> Alan's Farm Stand</h1>
+		Hello everyone. 
+		<?php
+			$current_time =  date("r");
+			echo "<br>". "Current time is $current_time"."<br>";
+			date_default_timezone_set("EST");
+			/*date("l"): A full textual representation of the day of the week*/
+			if (date("l") == "Saturday" || date("l") == "Sunday"){
+				echo "Today the store is open from 9 AM - 9 PM";
+				}
+			if (date("l") == "Monday"){
+				echo "Today we're close.";
+				}
+			else{
+				/*date("F"): A full textual representation of the month*/
+				if (date("F")=="July"|| date("F")=="August"){
+					echo "Today the store is open from 10 AM - 7 PM";
+					}
+				else{
+					echo "Today the store is open from 10 AM - 6 PM";
+					}
+				}
+			
+		?>
+	</body>
+	
+</html>
